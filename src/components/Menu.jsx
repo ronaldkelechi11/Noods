4eeb533385cd58b68a8004e31582ce3b89e21e60
Ronald Kelechi <ronaldkelechi11@gmail.com>
2023-12-29 20:41:42 +0100

    Corrected menu items and title

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -9,65 +9,79 @@ const Menu = () => {
                 "id": 1,
                 "name": "Noodles and Chicken",
                 "price": "2,900",
-                "image": "/src/assets/1.jpg",
-                "description": "lorem ipsum dolor inti bla bla bla"
+                "image": "/src/assets/images/1.jpg",
+                "description": "Chicken and Noods in one plate"
             },
             {
                 "id": 2,
                 "name": "Jollof Noodles and Chicken",
                 "price": "3,000",
-                "image": "/src/assets/1.jpg",
-                "description": "lorem ipsum dolor inti bla bla bla"
+                "image": "/src/assets/images/1.jpg",
+                "description": "Supmtious Nigerian Jollof Noodles"
             },
             {
                 "id": 3,
                 "name": "Chinese Noodles",
                 "price": "3,000",
-                "image": "/src/assets/2.jpg",
-                "description": "lorem ipsum dolor inti bla bla bla"
+                "image": "/src/assets/images/2.jpg",
+                "description": "Explore our Intercontinental recipe"
             },
             {
                 "id": 4,
                 "name": "Garnished Beef Noodles",
                 "price": "2,900",
-                "image": "/src/assets/1.jpg",
-                "description": "lorem ipsum dolor inti bla bla bla"
+                "image": "/src/assets/images/1.jpg",
+                "description": "Too sweet and Savoury"
             },
             {
                 "id": 5,
                 "name": "Creamy Pasta",
                 "price": "3,000",
-                "image": "/src/assets/1.jpg",
-                "description": "lorem ipsum dolor inti bla bla bla"
+                "image": "/src/assets/images/1.jpg",
+                "description": "Explore Special Creamy Macaronni"
             },
             {
                 "id": 6,
                 "name": "Peppered Noodles and Chicken",
                 "price": "3,000",
-                "image": "/src/assets/2.jpg",
-                "description": "lorem ipsum dolor inti bla bla bla"
+                "image": "/src/assets/images/2.jpg",
+                "description": "Ohh that's spicy"
             },
             {
                 "id": 7,
                 "name": "Garnished Macaroni and Chicken",
                 "price": "2,500",
-                "image": "/src/assets/1.jpg",
-                "description": "lorem ipsum dolor inti bla bla bla"
+                "image": "/src/assets/images/1.jpg",
+                "description": "Sweet and Yummy"
             },
             {
                 "id": 8,
                 "name": "Spicy Suya Noodles",
                 "price": "3,000",
-                "image": "/src/assets/1.jpg",
-                "description": "lorem ipsum dolor inti bla bla bla"
+                "image": "/src/assets/images/1.jpg",
+                "description": "Peppered Noodles with Suya Chunks"
             },
             {
                 "id": 9,
                 "name": "Native Noodles",
                 "price": "1,900",
-                "image": "/src/assets/1.jpg",
-                "description": "lorem ipsum dolor inti bla bla bla"
-            }
+                "image": "/src/assets/images/1.jpg",
+                "description": "A taste of special African Delicacy"
+            },
+            {
+                "id": 10,
+                "name": "Oriental Noodles and Chicken",
+                "price": "1,900",
+                "image": "/src/assets/images/1.jpg",
+                "description": "Intercontinental Recipe"
+            },
+            {
+                "id": 11,
+                "name": "Garnished Spaghetti and Chicken",
+                "price": "1,900",
+                "image": "/src/assets/images/1.jpg",
+                "description": "Sweet and Yummy"
+            },
         ]
     )
     return (
@@ -87,9 +101,10 @@ const Menu = () => {
 
 function MenuItem({ menuitem }) {
     return (
-        <div className="h-80 flex flex-col">
-            <img src={menuitem.image} className="h-[65%] w-full" />
-            <div className="w-full flex flex-col gap-1 justify-between p-2 text-center">
+        <div className="flex flex-col h-80">
+            <img src={menuitem.image} className="h-[60%] w-full" />
+
+            <div className="w-full h-full flex flex-col gap-1 justify-between p-2 text-center">
                 <p className="text-3xl md:text-2xl font-billabong">{menuitem.name}</p>
                 <p className="text-[11px] font-poppins text-slate-400">{menuitem.description}</p>
                 <p className="font-extrabold">N{menuitem.price}</p>
