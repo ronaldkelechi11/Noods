d44d5ac1c5a8ac687f025500b71f90c1046d532c
Ronald Kelechi <ronaldkelechi11@gmail.com>
2023-12-30 11:33:49 +0100

    try 4

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -9,31 +9,83 @@ const Menu = () => {
         [
             {
                 "id": 1,
-                "name": "Placeholder",
-                "price": "9,999",
-                "image": "",
-                "description": "placeholder"
+                "name": "Noodles and Chicken",
+                "price": "2,900",
+                "image": "/src/assets/images/1.jpg",
+                "description": "Chicken and Noods in one plate"
             },
             {
                 "id": 2,
-                "name": "Placeholder",
-                "price": "9,999",
-                "image": "",
-                "description": "placeholder"
+                "name": "Jollof Noodles and Chicken",
+                "price": "3,000",
+                "image": "/src/assets/images/1.jpg",
+                "description": "Supmtious Nigerian Jollof Noodles"
+            },
+            {
+                "id": 3,
+                "name": "Chinese Noodles",
+                "price": "3,000",
+                "image": "/src/assets/images/2.jpg",
+                "description": "Explore our Intercontinental recipe"
+            },
+            {
+                "id": 4,
+                "name": "Garnished Beef Noodles",
+                "price": "2,900",
+                "image": "/src/assets/images/1.jpg",
+                "description": "Too sweet and Savoury"
+            },
+            {
+                "id": 5,
+                "name": "Creamy Pasta",
+                "price": "3,000",
+                "image": "/src/assets/images/1.jpg",
+                "description": "Explore Special Creamy Macaronni"
+            },
+            {
+                "id": 6,
+                "name": "Peppered Noodles and Chicken",
+                "price": "3,000",
+                "image": "/src/assets/images/2.jpg",
+                "description": "Ohh that's spicy"
+            },
+            {
+                "id": 7,
+                "name": "Garnished Macaroni and Chicken",
+                "price": "2,500",
+                "image": "/src/assets/images/1.jpg",
+                "description": "Sweet and Yummy"
+            },
+            {
+                "id": 8,
+                "name": "Spicy Suya Noodles",
+                "price": "3,000",
+                "image": "/src/assets/images/1.jpg",
+                "description": "Peppered Noodles with Suya Chunks"
+            },
+            {
+                "id": 9,
+                "name": "Native Noodles",
+                "price": "1,900",
+                "image": "/src/assets/images/1.jpg",
+                "description": "A taste of special African Delicacy"
+            },
+            {
+                "id": 10,
+                "name": "Oriental Noodles and Chicken",
+                "price": "1,900",
+                "image": "/src/assets/images/1.jpg",
+                "description": "Intercontinental Recipe"
+            },
+            {
+                "id": 11,
+                "name": "Garnished Spaghetti and Chicken",
+                "price": "1,900",
+                "image": "/src/assets/images/1.jpg",
+                "description": "Sweet and Yummy"
             }
         ]
     )
-
-    useEffect(() => {
-        axios.get(menuitemUrl, { method: "GET" })
-            .then((result) => {
-                setItems(result.data)
-            }).catch((err) => {
-                console.log(err);
-            });
-
-    }, [])
-
 
     return (
         <div className="w-screen flex flex-col p-3">
@@ -41,7 +93,7 @@ const Menu = () => {
 
             <div className="md:grid md:grid-cols-4 gap-5 flex flex-col">
                 {
-                    items.map(menuitem => {
+                    items?.map(menuitem => {
                         return (<MenuItem menuitem={menuitem} key={menuitem.id} />)
                     })
                 }
