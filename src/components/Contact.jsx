764c01116636d1db76be12dfaeefb7b50387ee69
Ronald Kelechi <ronaldkelechi11@gmail.com>
2024-01-18 20:20:25 +0100

    still working on message feature

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,3 +1,4 @@
+import axios from "axios"
 import { useState } from "react"
 
 const Contact = () => {
@@ -5,17 +6,40 @@ const Contact = () => {
     const [email, setEmail] = useState("")
     const [telephone, setTelephone] = useState("")
     const [message, setMessage] = useState("")
+    const [marketer, setMarketer] = useState("")
 
-    function submitContactForm() {
+    const apiUrl = 'https://formsubmit.co/ronaldkelechi11@gmail.com'
 
+    function submitContactForm(e) {
+        e.preventDefault();
+
+        var newFormData = new FormData()
+        newFormData.append('name', name)
+        newFormData.append('email', email)
+        newFormData.append('telephone', telephone)
+        newFormData.append('refferal', marketer)
+        newFormData.append('message', message)
+
+        axios.post('https://formsubmit.co/ronaldkelechi11@gmail.com', {
+            method: 'post',
+            data: newFormData,
+        }).then((result) => {
+            console.log(result);
+            alert('Message Sent')
+        }).catch((err) => {
+            console.log(err);
+            alert('Error sending Mail. Please try Again Later')
+        });
     }
+
+
     return (
         <div id="contact" className="w-screen flex flex-col p-3">
             <div className="text-black text-center uppercase font-poppins text-3xl md:text-5xl">Contact Us</div>
             <div className="text-gray-400 text-sm font-poppins text-center">Feel free to contact us to order some noods.</div>
 
 
-            <div className="w-full h-full flex flex-col justify-evenly">
+            <form onSubmit={submitContactForm} className="w-full h-full flex flex-col justify-evenly">
 
                 <input className="h-[60px] p-2 font-poppins border-b border-gray-300 text-lg text-black placeholder:text-gray-400 outline-none" type="text" placeholder="Full Name" value={name} onChange={(e) => setName(e.target.value)} />
 
@@ -24,11 +48,15 @@ const Contact = () => {
 
                 <input className="h-[60px] p-2 font-poppins border-b border-gray-300 text-lg text-black placeholder:text-gray-400 outline-none" type="tel" placeholder="Telephone" value={telephone} onChange={(e) => setTelephone(e.target.value)} />
 
-                <textarea className="h-[90px] p-2 font-poppins border-b border-gray-300 text-lg text-black placeholder:text-gray-400 outline-none" cols="8" placeholder="Message"
-                    value={message} onChange={(e) => setMessage(e.target.value)}></textarea>
+                <input className="h-[60px] p-2 font-poppins border-b border-gray-300 text-lg text-black placeholder:text-gray-400 outline-none" type="tel" placeholder="Refferal Name" value={marketer} onChange={(e) => setMarketer(e.target.value)} />
 
-                <button className="bg-primary h-[60px] uppercase rounded-xl shadow-sm font-extrabold text-white" onClick={submitContactForm} type="submit">Done</button>
-            </div>
+                <textarea className="h-[90px] p-2 font-poppins border-b border-gray-300 text-lg text-black placeholder:text-gray-400 outline-none" cols="8" placeholder="Message"
+                    value={message} onChange={(e) => setMessage(e.target.value)}>
+
+                </textarea>
+
+                <button className="bg-primary h-[60px] uppercase rounded-xl shadow-sm font-extrabold text-white" type="submit">Done</button>
+            </form>
 
             <div className="w-full p-3">
                 <a href="https://wa.me/+2349136309200" className="flex flex-row items-center justify-center cursor-pointer">
